a44488ac442f4a460a30a758d038ae33cda2b0b6
Rashmita <rautrk1242@gmail.com>
2023-09-01 16:46:59 +0530

    Card UI

--- app/src/main/java/com/example/thenewsroom/ui/theme/news_screen/NewsScreen.kt
+++ app/src/main/java/com/example/thenewsroom/ui/theme/news_screen/NewsScreen.kt
@@ -2,24 +2,45 @@ package com.example.thenewsroom.ui.theme.news_screen
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.thenewsroom.ui.theme.component.NewsArticleCard
+import com.example.thenewsroom.ui.theme.component.NewsScreenTopBar
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NewsScreen(
     viewModel: NewsScreenViewModel = hiltViewModel()
 ){
-    LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        contentPadding = PaddingValues(16.dp)
-    ){
-        items(viewModel.articles){article ->
-            Text(text = article.title)
-
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = { NewsScreenTopBar(
+            scrollBehavior = scrollBehavior,
+            onSearchIconClicked = {}) }
+    ) { padding ->
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(16.dp),
+            modifier = Modifier.padding(padding )
+        ){
+            items(viewModel.articles){article ->
+                NewsArticleCard(
+                    article = article,
+                    onCardClicked = { }
+                )
+            }
         }
     }
+
 }
